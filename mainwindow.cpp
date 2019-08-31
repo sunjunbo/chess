42cde1248ed2899c3b6cd8fa0840c0b67ece8bc4
TongYuchang <tyc16@qq.com>
2019-09-01 00:36:06 +0800

    a

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,6 +24,20 @@ void MainWindow::init(){
     int width = 60;
     int x=50;
     int y=50;
+
+    icons[1][1]=QIcon(":/c/pic/black_rook.png");// 车
+    icons[1][2]=QIcon(":/c/pic/black_knight.png");// 马
+    icons[1][3]=QIcon(":/c/pic/black_bishop.png");// 象
+    icons[1][4]=QIcon(":/c/pic/black_king.png");// 王
+    icons[1][5]=QIcon(":/c/pic/black_queen.png");// 后
+    icons[1][6]=QIcon(":/c/pic/black_pawn.png");// 兵
+
+    icons[2][1]=QIcon(":/c/pic/white_rook.png");
+    icons[2][2]=QIcon(":/c/pic/white_knight.png");
+    icons[2][3]=QIcon(":/c/pic/white_bishop.png");
+    icons[2][4]=QIcon(":/c/pic/white_king.png");
+    icons[2][5]=QIcon(":/c/pic/white_queen.png");
+    icons[2][6]=QIcon(":/c/pic/white_pawn.png");
     QGridLayout *layout = ui->gridLayout;
     layout->setSpacing(0);
     for(int i=1;i<=8;i++)
@@ -50,9 +64,29 @@ void MainWindow::on_action_2_triggered()
 }
 
 void MainWindow::set_host()
-{
+{   
     //作为服务器初始化，小红帽写
     //此时双方链接已经建立
+    for(int i=1;i<=4;i++)
+    {
+        ButtonMap[1][i]->setIcon(icons[1][i]);
+        ButtonMap[1][8-i]->setIcon(icons[1][i]);
+    }
+    ButtonMap[1][5]->setIcon(icons[1][5]);
+    for(int i=1;i<=8;i++)
+    {
+        ButtonMap[2][i]->setIcon(icons[1][6]);
+    }
+    for(int i=1;i<=4;i++)
+    {
+        ButtonMap[8][i]->setIcon(icons[2][i]);
+        ButtonMap[8][8-i]->setIcon(icons[2][i]);
+    }
+    ButtonMap[8][5]->setIcon(icons[2][5]);
+    for(int i=1;i<=8;i++)
+    {
+        ButtonMap[7][i]->setIcon(icons[2][6]);
+    }
 }
 
 void MainWindow::set_client()
